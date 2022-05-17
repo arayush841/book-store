a3012ddda906633cd6ffc958617d60eca9bee12b
Ayush Raj <arayush841@gmail.com>
2022-05-17 19:10:39 +0530

    fixed about.php

--- about.php
+++ about.php
@@ -19,7 +19,7 @@ if (!isset($user_id)) {
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>about</title>
+   <title>About Us</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
@@ -46,7 +46,7 @@ if (!isset($user_id)) {
 
    <div class=" heading">
       <h3>about us</h3>
-      <p> <a href="home.php">home</a> / about </p>
+      
    </div>
 
    <section class="about">
